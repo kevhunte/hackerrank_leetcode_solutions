80a3654e202da1ed9416ec6bf6783355a2ff64b1
kevhunte <kevinmhunte@gmail.com>
2021-04-18 10:30:15 -0400

    optimization

--- leetcode/second_highest_salary.sql
+++ leetcode/second_highest_salary.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 Select Max(Salary) as SecondHighestSalary
 From Employee
-Where Salary not in (Select Max(Salary) from Employee)
+Where Salary < (Select Max(Salary) from Employee)
